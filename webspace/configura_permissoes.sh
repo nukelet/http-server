a20cf0cdd347cb3b71a80cd9d3cea155a8eadb92
nukelet <nukelet@protonmail.com>
2022-12-09 14:25:11 -0300

    Add test webspace

---
+++ webspace/configura_permissoes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#muda as permissoes dos arquivos e diretorios
+chmod u-r teste.txt
+chmod u-x ./dir
+chmod u-r ./dir3/index.html
+chmod u-r ./dir4/index.html
+chmod u-r ./dir4/welcome.html
